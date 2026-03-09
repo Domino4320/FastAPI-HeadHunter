27bf0752b977d5760b6a5089c069d1e98933a5ca
Ruslan <pprrg18@gmail.com>
2026-03-09 14:50:30 +0300

    crud patch and delete

--- resumes/crud.py
+++ resumes/crud.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, Sequence
+from sqlalchemy import select, Sequence, delete, update
 from core.models.resume import Resume
 from resumes.schemas import ResumePostSchema
 from sqlalchemy.orm import joinedload
+from typing import Any
 
 
 class ResumeProcessor:
@@ -20,3 +21,18 @@ class ResumeProcessor:
         resume = Resume(**data.model_dump())
         session.add(resume)
         await session.commit()
+
+    @staticmethod
+    async def delete_resume_from_db(session: AsyncSession, resume_id: int) -> None:
+        query = delete(Resume).where(Resume.id == resume_id)
+        await session.execute(query)
+        await session.commit()
+
+    @staticmethod
+    async def patch_resume_into_db(
+        session: AsyncSession, resume_id: int, new_data: dict[str, Any]
+    ):
+        query = update(Resume).values(**new_data).where()
+        result = await session.execute(query)
+        await session.commit()
+        return result.rowcount
